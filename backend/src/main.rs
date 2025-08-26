b27c6098877e1e54a59910d55c72a371151a9fd8
sambiit01 <sambitbhoumik01@gmail.com>
2025-08-27 00:20:38 +0530

    feat(backend): add SQLx integration, migrations, pool factory, health probe, developer docs (#469)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+
+mod db;
+use anyhow::Result;
+use dotenvy::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL")?;
+    let pool = crate::db::DbPool::new(&database_url).await?;
+    pool.health_check().await?;
+    // Add code here to launch server, pass pool to handlers, etc.
+    Ok(())
 }
